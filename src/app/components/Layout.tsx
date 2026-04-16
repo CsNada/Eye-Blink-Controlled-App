3e78060d668d73531fc80b518571e7eb1067214f
CsNada <nada.alrajhi.2020@gmail.com>
2026-04-16 15:22:00 +0300

    update

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -8,22 +8,15 @@ export function Layout() {
   const blinkContext = useBlink();
   const { seconds, message, isTracking, isFaceDetected } = useEyeContext();
 
-  const showDeleteButton = blinkContext?.showDeleteButton ?? true;
-  const showSendButton = blinkContext?.showSendButton ?? true;
-
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Header />
-
 
       <main className="pb-72">
         <Outlet />
       </main>
 
-      <BlinkControls 
-        showDeleteButton={showDeleteButton}
-        showSendButton={showSendButton}
-      />
+      <BlinkControls />
     </div>
   );
 }
