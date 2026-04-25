a33b63a0d095c62dc0851a82f61645843fad2cd0
CsNada <nada.alrajhi.2020@gmail.com>
2026-04-25 11:21:07 +0300

    update

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import { Header } from "./Header";
 import { BlinkControls } from "./BlinkControls";
+import ScrollToTop from "./ScrollToTop";
 
 export function Layout() {
   return (
     <div className="min-h-screen bg-background text-foreground">
+      <ScrollToTop />
       <Header />
 
       <main className="pb-72">
